870b66482d2ce56e44f566101be0bd7385f2d923
Carl Kakisis <Messinias1@gmail.com>
2019-06-11 21:46:38 -0400

    psuedo code added in

--- sumAllNumsInaRange.js
+++ sumAllNumsInaRange.js
@@ -1,13 +1,16 @@
 function sumAll(arr) {
+  // get the smallest and biggest numbers in the array
     var max = Math.max(...arr);
     var min = Math.min(...arr);
-  
+
+  // look for numbers between the smallest and largest number inclusively
     var total = 0;
     for (var i = min; i <= max; i++) {
+      // sum all of the numbers
       total += i;
     }
     console.log(total)
     return total;
   }
   
-  sumAll([1, 4, 7, 8]);
+  sumAll([1, 3]);
