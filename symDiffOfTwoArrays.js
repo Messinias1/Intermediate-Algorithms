ae96791f627f5d8394288fd61476eb40ca71d784
Carl Kakisis <Messinias1@gmail.com>
2019-06-13 12:52:02 -0400

    added a new solution

--- symDiffOfTwoArrays.js
+++ symDiffOfTwoArrays.js
@@ -1,18 +1,32 @@
 function diffArray(arr1, arr2) {
     var newArr = [];
   
-    function compare(first, second) {
-      console.log(second)
-      for (var i = 0; i < first.length; i++) {
-        if (second.indexOf(first[i]) === -1) {
-          newArr.push(first[i])
-        }
-        // console.log(first[i])
-      }
-    }
-    compare(arr1, arr2)
-    compare(arr2, arr1)
-    console.log(newArr)
+    // function compare(first, second) {
+    //   console.log(second)
+    //   for (var i = 0; i < first.length; i++) {
+    //     if (second.indexOf(first[i]) === -1) {
+    //       newArr.push(first[i])
+    //     }
+    //     // console.log(first[i])
+    //   }
+    // }
+    // compare(arr1, arr2)
+    // compare(arr2, arr1)
+    // console.log(newArr)
+
+    // for (var i = 0; i < arr1.length; i++) {
+    //   if (arr2.indexOf(arr1[i]) === -1) {
+    //     newArr.push(arr1[i])
+    //   }
+    // }
+
+    // for (var i = 0; i < arr2.length; i++) {
+    //   if (arr1.indexOf(arr2[i]) === -1) {
+    //     newArr.push(arr2[i])
+    //   }
+    // }
+
+    
     return newArr
   }
   
