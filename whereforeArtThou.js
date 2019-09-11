a5b06f9450f38b2f0ad4264b788eddb151609359
Carl Kakisis <Messinias1@gmail.com>
2019-09-11 13:24:06 -0400

    closer to solution

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -1,17 +1,24 @@
 function whatIsInAName(collection, source) {
-    // What's in a name?
-    var arr = [];
-    // Only change code below this line
-    const keys = Object.keys(source)
-  
-    arr = collection.filter(element => {
-      return keys.every(key => {
-        return element[key] && element[key] === source[key]
-      })
-    })
-    
-    // Only change code above this line
-    return arr;
-  }
-  
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  const keys = Object.keys(source);
+
+  arr = collection.filter(element => {
+    return keys.every(key => {
+      return element[key] && element[key] === source[key];
+    });
+  });
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
