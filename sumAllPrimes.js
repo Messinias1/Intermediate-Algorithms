c7d7c3cc8b7eefaa45761049333f458a916c76a6
Carl Kakisis <Messinias1@gmail.com>
2019-09-14 15:54:13 -0400

    sumAllPrimes solution

--- sumAllPrimes.js
+++ sumAllPrimes.js
@@ -1,5 +1,23 @@
 function sumPrimes(num) {
-  return num;
+  var sieve = [],
+    i,
+    j,
+    primes = [];
+  for (i = 2; i <= num; ++i) {
+    if (!sieve[i]) {
+      // i has not been marked -- it is prime
+      primes.push(i);
+      for (j = i << 1; j <= num; j += i) {
+        sieve[j] = true;
+      }
+    }
+  }
+  function getSum(total, num) {
+    return total + num;
+  }
+  console.log(primes.reduce(getSum));
+
+  return primes.reduce(getSum);
 }
 
-sumPrimes(10);
+sumPrimes(977);
