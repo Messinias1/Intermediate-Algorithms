ff9075d226b2f507ca69aae1039640850897efcd
Carl Kakisis <Messinias1@gmail.com>
2019-09-19 01:57:38 -0400

    century from year solution

---
+++ codeSignal/centuryFromYear.js
@@ -0,0 +1,87 @@
+function centuryFromYear(year) {
+  if (year >= 1 && year < 100) {
+    console.log(1);
+    return 1;
+  }
+  if (year >= 101 && year <= 200) {
+    console.log(2);
+    return 2;
+  }
+  if (year >= 201 && year <= 300) {
+    console.log(3);
+    return 3;
+  }
+  if (year >= 301 && year <= 400) {
+    console.log(4);
+    return 4;
+  }
+  if (year >= 401 && year <= 500) {
+    console.log(5);
+    return 5;
+  }
+  if (year >= 501 && year <= 600) {
+    console.log(6);
+    return 6;
+  }
+  if (year >= 601 && year <= 700) {
+    console.log(7);
+    return 7;
+  }
+  if (year >= 701 && year <= 800) {
+    console.log(8);
+    return 8;
+  }
+  if (year >= 801 && year <= 900) {
+    console.log(9);
+    return 9;
+  }
+  if (year >= 901 && year <= 1000) {
+    console.log(10);
+    return 10;
+  }
+  if (year >= 1001 && year <= 1100) {
+    console.log(11);
+    return 11;
+  }
+  if (year >= 1101 && year <= 1200) {
+    console.log(12);
+    return 12;
+  }
+  if (year >= 1201 && year <= 1300) {
+    console.log(13);
+    return 13;
+  }
+  if (year >= 1301 && year <= 1400) {
+    console.log(14);
+    return 14;
+  }
+  if (year >= 1401 && year <= 1500) {
+    console.log(15);
+    return 15;
+  }
+  if (year >= 1501 && year <= 1600) {
+    console.log(16);
+    return 16;
+  }
+  if (year >= 1601 && year <= 1700) {
+    console.log(17);
+    return 17;
+  }
+  if (year >= 1701 && year <= 1800) {
+    console.log(18);
+    return 18;
+  }
+  if (year >= 1801 && year <= 1900) {
+    console.log(19);
+    return 19;
+  }
+  if (year >= 1901 && year <= 2000) {
+    console.log(20);
+    return 20;
+  }
+  if (year >= 2001 && year <= 2100) {
+    console.log(21);
+    return 21;
+  }
+}
+centuryFromYear(1700);
