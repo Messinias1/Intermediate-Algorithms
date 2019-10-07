95914ffcdcda92a388c634b974eb38d613c5cd30
Carl Kakisis <Messinias1@gmail.com>
2019-10-06 20:49:56 -0400

    pigLatin2.0 solution

--- intermediate/pigLatin2.0.js
+++ intermediate/pigLatin2.0.js
@@ -1,5 +1,25 @@
 function translatePigLatin(str) {
-  return str;
+  var vowel = /[aeiou]/gi;
+
+  if (str[0].match(vowel)) {
+    return (str = str + "way");
+  } else if (str.match(vowel)) {
+    var index = str.indexOf(str.match(vowel)[0]);
+    return (str =
+      str
+        .split("")
+        .slice(index)
+        .join("") +
+      str.slice(0, index) +
+      "ay");
+  } else return str + "ay";
 }
 
-translatePigLatin("consonant");
+// translatePigLatin("paragraphs"); // should return "aragraphspay".
+// translatePigLatin("apple");
+//   translatePigLatin("eight");
+translatePigLatin("pro"); // opray
+
+// Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+// If a word begins with a vowel you just add "way" to the end.
+// Input strings are guaranteed to be English words in all lowercase
--- intermediate/test.js
+++ intermediate/test.js
@@ -1,32 +1,40 @@
-function Person(name, lastName) {
-  this.name = name;
-  this.lastName = lastName;
+function translatePigLatin(str) {
+  var vowel = /[aeiou]/gi;
+  console.log(str.match(vowel));
 
-  this.updateName = function(newName) {
-    return (this.name = newName);
-  };
+  if (str[0].match(vowel)) {
+    console.log((str = str + "way"));
+    return (str = str + "way");
+  }
 
-  this.updateLastName = function(newLastName) {
-    return (this.lastName = newLastName);
-  };
-
-  this.updateFullName = function(firstAndLast) {
-    return (this.getFullName = firstAndLast);
-  };
-
-  this.getName = function() {
-    return this.name;
-  };
-
-  this.getLastName = function() {
-    return this.lastName;
-  };
-
-  this.getFullName = function() {
-    return this.name + this.lastName;
-  };
+  if (str.match(vowel)) {
+    var index = str.indexOf(str.match(vowel)[0]);
+    console.log(index);
+    console.log(str.slice(0, index));
+    console.log(
+      (str =
+        str
+          .split("")
+          .slice(index)
+          .join("") +
+        str.slice(0, index) +
+        "ay")
+    );
+    return (str =
+      str
+        .split("")
+        .slice(index)
+        .join("") +
+      str.slice(0, index) +
+      "ay");
+  } else return str + "ay";
 }
 
-let Carl = new Person("Carl", "Kakisis");
-console.log(Carl.updateFullName("Sven Ostman"));
-console.log(Carl.getFullName());
+// translatePigLatin("paragraphs"); // should return "aragraphspay".
+// translatePigLatin("apple");
+translatePigLatin("sly");
+// translatePigLatin("pro"); // opray
+
+// Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+// If a word begins with a vowel you just add "way" to the end.
+// Input strings are guaranteed to be English words in all lowercase
