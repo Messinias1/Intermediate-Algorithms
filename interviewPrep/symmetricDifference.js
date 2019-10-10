5ed093bd9dd051d1f690e2b7db8b873fbbf326b0
Carl Kakisis <Messinias1@gmail.com>
2019-10-10 00:00:19 -0400

    symmetricDifference solution

--- interviewPrep/symmetricDifference.js
+++ interviewPrep/symmetricDifference.js
@@ -49,18 +49,93 @@ function sym(args) {
     }
     console.log(argsArr);
     // array1 = array1.filter(val => !array2.includes(val));
-    argsArr.reduce((acc, compare) => {
-      var merger = acc.concat(compare);
-      var newStart = merger.filter(item => {
-        return merger.lastIndexOf(item) == merger.indexOf(item);
-      });
-      console.log(newStart);
-    });
+    var filteredArr = [];
+    for (var i = 0; i < argsArr.length; i++) {
+      if (i < argsArr.length - 1) {
+        var filtered = argsArr[i].concat(argsArr[i + 1]).filter(item => {
+          return (
+            argsArr[i].concat(argsArr[i + 1]).lastIndexOf(item) ==
+            argsArr[i].concat(argsArr[i + 1]).indexOf(item)
+          );
+        });
+        filteredArr.push(filtered);
+        // console.log(
+        //   filtered.filter(item => {
+        //     return filtered.lastIndexOf(item) == filtered.indexOf(item);
+        //   })
+        // );
+        i++;
+      }
+    }
+
+    console.log(filteredArr);
+    for (var i = 0; i < filteredArr.length; i = i + 2) {
+      if (i < filteredArr.length - 1) {
+        var final = filteredArr[i].concat(filteredArr[i + 1]).filter(item => {
+          return (
+            filteredArr[i].concat(filteredArr[i + 1]).lastIndexOf(item) ==
+            filteredArr[i].concat(filteredArr[i + 1]).indexOf(item)
+          );
+        });
+        console.log(final);
+        return final;
+      }
+    }
+    // argsArr.reduce((acc, compare) => {
+    //   console.log(acc);
+    //   var merger = acc.concat(compare);
+    //   var newStart = merger.filter(item => {
+    //     return merger.lastIndexOf(item) == merger.indexOf(item);
+    //   });
+    //   console.log(newStart);
+    // });
+  }
+
+  if ([...arguments].length == 6) {
+    var argsArr = [];
+    for (var i = 0; i < [...arguments].length; i++) {
+      var tester = Array.from(new Set([...arguments][i]));
+      argsArr.push(tester);
+    }
+    console.log(argsArr);
+    // array1 = array1.filter(val => !array2.includes(val));
+    var filteredArr = [];
+    for (var i = 0; i < argsArr.length; i++) {
+      if (i < argsArr.length - 1) {
+        var filtered = argsArr[i].concat(argsArr[i + 1]).filter(item => {
+          return (
+            argsArr[i].concat(argsArr[i + 1]).lastIndexOf(item) ==
+            argsArr[i].concat(argsArr[i + 1]).indexOf(item)
+          );
+        });
+        filteredArr.push(filtered);
+        i++;
+      }
+    }
+
+    console.log(filteredArr);
+    for (var i = 0; i < filteredArr.length; i = i + 2) {
+      if (i < filteredArr.length - 1) {
+        var final = filteredArr[i].concat(filteredArr[i + 1]).filter(item => {
+          return (
+            filteredArr[i].concat(filteredArr[i + 1]).lastIndexOf(item) ==
+            filteredArr[i].concat(filteredArr[i + 1]).indexOf(item)
+          );
+        });
+
+        var concatted = final.concat(filteredArr[2]);
+        console.log(concatted);
+        var end = concatted.filter(item => {
+          return concatted.lastIndexOf(item) == concatted.indexOf(item);
+        });
+        console.log(end);
+        return end;
+      }
+    }
   }
 }
 
-sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]); // [2, 3, 4, 6, 7]
-// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]); // [1, 2, 4, 5, 6, 7, 8, 9]
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]); // [1, 2, 4, 5, 6, 7, 8, 9]
 
 // Given two sets (for example set A = {1, 2, 3}and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}). The resulting array must contain only unique values (no duplicates).
 
