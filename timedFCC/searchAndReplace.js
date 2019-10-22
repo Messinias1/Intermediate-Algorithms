2d392177c051ea2f4b13deb423dd2d259fdc94cf
Carl Kakisis <Messinias1@gmail.com>
2019-10-22 11:33:44 -0400

    timed solution for search and replace

---
+++ timedFCC/searchAndReplace.js
@@ -0,0 +1,36 @@
+function myReplace(str, before, after) {
+  console.log(before[0]);
+
+  var lower = /[a-z]/;
+  var upper = /[A-Z]/;
+
+  if (before[0].match(lower)) {
+    console.log(
+      (str = str.replace(
+        before,
+        after[0].toLowerCase() + after.substring(1, after.length)
+      ))
+    );
+    return (str = str.replace(
+      before,
+      after[0].toLowerCase() + after.substring(1, after.length)
+    ));
+  }
+  if (before[0].match(upper)) {
+    console.log(
+      (str = str.replace(
+        before,
+        after[0].toUpperCase() + after.substring(1, after.length)
+      ))
+    );
+    return (str = str.replace(
+      before,
+      after[0].toUpperCase() + after.substring(1, after.length)
+    ));
+  }
+  //   console.log((str = str.replace(before, after)));
+}
+
+myReplace("His name is Tom", "Tom", "john");
+
+// 7m 19s
