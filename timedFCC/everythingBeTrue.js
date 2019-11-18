5b80dd2a0b5aa32fe49718412dccb25d06076bfd
Carl Kakisis <Messinias1@gmail.com>
2019-11-17 21:43:51 -0500

    everything be true timed algo solved

--- timedFCC/everythingBeTrue.js
+++ timedFCC/everythingBeTrue.js
@@ -1,51 +1,37 @@
 function truthCheck(collection, pre) {
-  var result = true;
   for (var i = 0; i < collection.length; i++) {
-    if (!Object.keys(collection[i]).includes(pre)) {
-      result = false;
-    }
+    // console.log(Object.entries(collection[i]).flat());
+    if (
+      Object.entries(collection[i])
+        .flat()
+        .includes(pre)
+    ) {
+      console.log("true");
+    } else return false;
   }
-  for (var j = 0; j < collection.length; j++) {
-    console.log(Object.values(collection[j]));
-    switch (Object.values(collection[j])) {
-      case false:
-        result = false;
-        break;
-      case undefined:
-        result = false;
-        break;
-      case null:
-        result = false;
-        break;
-      case 0:
-        result = false;
-        break;
-      case "":
-        result = false;
-        break;
-      case Object.values(collection[j]).includes(NaN):
-        result = false;
-        break;
-      default:
-        return result;
-    }
-    // if (
-    //   Object.values(collection[j]) !== false ||
-    //   Object.values(collection[j]) !== undefined ||
-    //   Object.values(collection[j]) !== null ||
-    //   Object.values(collection[j]) !== 0 ||
-    //   Object.values(collection[j]) !== "" ||
-    //   Object.values(collection[j]).includes(NaN)
-    // ) {
-    //   return true;
-    // } else {
-    //   console.log("false");
-    // }
+  var arr = [];
+  for (var el = 0; el < collection.length; el++) {
+    arr.push(Object.entries(collection[el]));
   }
-  return result;
+  console.log(arr.flat(2));
+  if (
+    arr.flat(2).includes(undefined) ||
+    arr.flat(2).includes(false) ||
+    arr.flat(2).includes(null) ||
+    arr.flat(2).includes(0) ||
+    arr.flat(2).includes("") ||
+    arr.flat(2).includes(NaN)
+  ) {
+    console.log("false");
+    return false;
+  } else {
+    console.log("true");
+    return true;
+  }
 }
 
-truthCheck([{ single: "double" }, { single: undefined }], "single");
+truthCheck([{ single: "double" }, { single: "" }], "single");
+// 24m
 
 // truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
 
