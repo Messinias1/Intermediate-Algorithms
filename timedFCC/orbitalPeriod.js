be0aa6fcee5f32557764a6f8ee1431dc5f23a096
Carl Kakisis <Messinias1@gmail.com>
2019-11-22 00:30:35 -0500

    orbital period timed solution

--- timedFCC/orbitalPeriod.js
+++ timedFCC/orbitalPeriod.js
@@ -1,10 +1,21 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  console.log(arr[0].avgAlt);
-  var timeInSeconds =
-    2 * Math.PI * Math.sqrt(Math.pow(earthRadius + arr[0].avgAlt, 3) / GM);
-  console.log(Math.round(timeInSeconds));
+
+  for (var i = 0; i < arr.length; i++) {
+    var timeInSeconds =
+      2 * Math.PI * Math.sqrt(Math.pow(earthRadius + arr[i].avgAlt, 3) / GM);
+    arr[i].avgAlt = arr[i].orbitalPeriod;
+    delete arr[i].avgAlt;
+    arr[i].orbitalPeriod = Math.round(timeInSeconds);
+  }
+  console.log(arr);
+  return arr;
 }
 
-orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+orbitalPeriod([
+  { name: "iss", avgAlt: 413.6 },
+  { name: "hubble", avgAlt: 556.7 },
+  { name: "moon", avgAlt: 378632.553 }
+]);
+// 6m 48s
