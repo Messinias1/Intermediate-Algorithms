5b12bfcb1e28cf592a07189c74cc2025e6197b65
Carl Kakisis <Messinias1@gmail.com>
2019-11-23 21:44:01 -0500

    changes to cash register

--- advancedAlgos/cashRegister.js
+++ advancedAlgos/cashRegister.js
@@ -1,44 +1,40 @@
 function checkCashRegister(price, cash, cid) {
-  let penny = 0.01;
-  let nickel = 0.05;
-  let dime = 0.1;
-  let quarter = 0.25;
-  let one = 1;
-  let five = 5;
-  let ten = 10;
-  let twenty = 20;
-  let hundred = 100;
+  // get total cid
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let total = parseFloat(
+    cid
+      .flat()
+      .filter(item => {
+        return typeof item === "number";
+      })
+      .reduce(reducer)
+      .toFixed(2)
+  );
+  console.log(total);
 
-  let change = cash - price;
-  let arr = [];
-  let cidTotal;
+  // get change
+  const CHANGE = cash - price;
+  console.log(CHANGE);
 
-  for (let i = 0; i < cid.length; i++) {
-    console.log(cid[i]);
-    arr.push(cid[i][1]);
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    cidTotal = Math.round(arr.reduce(reducer) * 100) / 100;
-  }
-  console.log(cidTotal);
-  //   console.log(price);
-  //   console.log(cash);
-  //   console.log(change);
-
-  if (cidTotal < change) {
-    console.log("no funds");
+  // if cid < CHANGE
+  if (total < CHANGE) {
+    console.log({ status: "INSUFFICIENT_FUNDS", change: [] });
     return { status: "INSUFFICIENT_FUNDS", change: [] };
-  } else if (cidTotal == change) {
-    console.log("even");
-    for (let i = 0; i < cid.length; i++) {
-      console.log(cid[i][1] == change);
-      console.log({ status: "CLOSED", change: cid[i] });
-      return { status: "CLOSED", change: cid[i] };
-    }
+    // if cid === CHANGE
+  } else if (total === CHANGE) {
+    console.log({
+      status: "CLOSED",
+      change: cid.filter(value => {
+        console.log(value.includes(CHANGE));
+        return value === value.includes(CHANGE);
+      })
+    });
   }
 }
+
 checkCashRegister(19.5, 20, [
-  ["PENNY", 0],
-  ["NICKEL", 0.5],
+  ["PENNY", 0.5],
+  ["NICKEL", 0],
   ["DIME", 0],
   ["QUARTER", 0],
   ["ONE", 0],
@@ -47,6 +43,17 @@ checkCashRegister(19.5, 20, [
   ["TWENTY", 0],
   ["ONE HUNDRED", 0]
 ]);
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 0.01],
+//   ["NICKEL", 0],
+//   ["DIME", 0],
+//   ["QUARTER", 0],
+//   ["ONE", 1],
+//   ["FIVE", 0],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 0]
+// ]);
 
 //   Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 // cid is a 2D array listing available currency.
