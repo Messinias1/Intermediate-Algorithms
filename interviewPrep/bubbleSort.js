09d0756c72d7b3d077abbd7f8b491a1a3384b72d
Carl Kakisis <Messinias1@gmail.com>
2019-11-26 21:33:49 -0500

    bubbleSort initialization

---
+++ interviewPrep/bubbleSort.js
@@ -0,0 +1,25 @@
+function bubbleSort(array) {
+  // change code below this line
+  console.log(array);
+  // change code above this line
+}
+
+bubbleSort([
+  1,
+  4,
+  2,
+  8,
+  345,
+  123,
+  43,
+  32,
+  5643,
+  63,
+  123,
+  43,
+  2,
+  55,
+  1,
+  234,
+  92
+]);
