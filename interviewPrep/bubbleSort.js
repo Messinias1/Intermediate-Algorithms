2d96ef51151f33f9514b44e8f34a4d6face11c69
Carl Kakisis <Messinias1@gmail.com>
2019-11-27 12:20:58 -0500

    bubbleSort solution

--- interviewPrep/bubbleSort.js
+++ interviewPrep/bubbleSort.js
@@ -1,6 +1,16 @@
 function bubbleSort(array) {
   // change code below this line
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
   console.log(array);
+  return array;
   // change code above this line
 }
 
