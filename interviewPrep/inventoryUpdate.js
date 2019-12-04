f6df4e0149380e9f7e371f38c5d1c7912df70676
Carl Kakisis <Messinias1@gmail.com>
2019-12-03 21:13:01 -0500

    near solution

--- interviewPrep/inventoryUpdate.js
+++ interviewPrep/inventoryUpdate.js
@@ -1,6 +1,27 @@
 function updateInventory(arr1, arr2) {
-  // All inventory must be accounted for or you're fired!
-  return arr1;
+  for (var i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+    for (var j = 0; j < arr2.length; j++) {
+      console.log("j", arr2[j][1]);
+      if (arr1[i][1] == arr2[j][1]) {
+        console.log((arr1[i][0] = arr1[i][0] + arr2[j][0]));
+      }
+    }
+  }
+  var concatted = arr1.concat(arr2);
+  concatted.sort(function(a, b) {
+    if (a[1] < b[1]) {
+      return -1;
+    }
+    if (b[1] < a[1]) {
+      return 1;
+    }
+    return 0;
+  });
+  var filtered = concatted.filter(function(item, index, inputArray) {
+    return inputArray.indexOf(item) == index;
+  });
+  console.log(filtered);
 }
 
 // Example inventory lists
@@ -19,3 +40,4 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
+// u should return [[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]]
