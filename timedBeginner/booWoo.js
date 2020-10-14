db8d6d21e12901cda45ec8129ea87ce336888235
Carl Kakisis <Messinias1@gmail.com>
2020-10-14 16:26:47 -0400

    test commit

--- timedBeginner/booWoo.js
+++ timedBeginner/booWoo.js
@@ -5,6 +5,6 @@ function booWho(bool) {
 booWho(null);
 
 // 18s
-
+// test comment
 // Check if a value is classified as a boolean primitive. Return true or false.
 // Boolean primitives are true and false.
